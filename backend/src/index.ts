dfe49f4db6df9a1b6222c97b073883c93892f557
souvik ghosh <xandersavy@gmail.com>
2021-09-06 23:41:03 +0530

    backend initialize

---
+++ backend/src/index.ts
@@ -0,0 +1,16 @@
+// Importing module
+import express from 'express';
+  
+const app = express();
+const PORT:Number=3000;
+  
+// Handling GET / Request
+app.get('/', (req, res) => {
+    res.send('Kelabo pratik Babe khaya');
+})
+  
+// Server setup
+app.listen(PORT,() => {
+    console.log('The application is listening '
+          + 'on port http://localhost:'+PORT);
+})
