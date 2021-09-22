f2c3337585038931f0a86290673c7f47978351a8
souvik ghosh <xandersavy@gmail.com>
2021-09-22 18:24:03 +0530

    app.ts index.ts created

---
+++ backend/src/app.ts
@@ -0,0 +1,75 @@
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import logger from 'morgan';
+import mongoose from 'mongoose';
+import express, { NextFunction, Response, Request } from 'express';
+require('dotenv').config();
+
+class App {
+    public app: express.Application;
+    public apiV1Routes: express.Router;
+
+    constructor() {
+        this.app = express();
+        this.apiV1Routes = express.Router();
+        this.initializeMiddlewares();
+        this.initializeLogger();
+        this.initializeErrorHandling();
+        this.routes();
+    }
+
+    public createDBConnection() {
+        mongoose.connect(process.env.MONGODB_URI,
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+                useFindAndModify: true,
+                useCreateIndex: true,
+                poolSize: Number(process.env.MONGODB_POOLSIZE),
+                keepAlive: true,
+            });
+    }
+
+    public listen() {
+        this.createDBConnection();
+        this.app.listen(process.env.PORT, () => {
+            console.log(`App listening on the port ${process.env.PORT}`);
+        });
+    }
+
+    public getServer() {
+        return this.app;
+    }
+
+    private initializeMiddlewares() {
+        this.app.use(cors());
+        this.app.use(bodyParser.json());
+        this.app.use(bodyParser.raw());
+        this.app.use(bodyParser.urlencoded({ extended: true }));
+        this.app.use(cookieParser());
+        this.app.use(logger('[:date[web]] :method :url :status :res[content-length] - :remote-addr - :response-time ms'));
+    }
+
+    private initializeErrorHandling() {
+
+    }
+
+    private initializeLogger() {
+        const LOG_PREFIX = new Date().getDate() + '.' + (new Date().getMonth() + 1) + '.' + new Date().getFullYear() + ' ' + new Date().getHours() + ':' + new Date().getMinutes() + ':' + new Date().getSeconds();
+        const log = console.log;
+        console.log = function () {
+            const args = Array.from(arguments);
+            args.unshift(LOG_PREFIX + ": ");
+            log.apply(console, args);
+        }
+    }
+
+    private routes() {
+        this.app.get('/', (req: Request, res: Response, next: NextFunction) => {
+            res.send('Senhigans Backend');
+        });
+        this.app.use('/api/v1', this.apiV1Routes);
+        this.apiV1Routes.use('/user', ); //enterroutes
+    }
+}
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,21 @@
 // Importing module
-import express from 'express';
-const dotenv = require('dotenv').config();  
-import cors from 'cors';
+// import express from 'express';
+import App from './app';
+require('dotenv').config();  
+// import cors from 'cors';
 
-const app = express();
-const PORT:Number=3000;
+const app = new App;
+const PORT : Number= 3000||process.env.PORT;
   
-// Handling GET / Request
-app.get('/', (req, res) => {
-    res.send('Kelabo pratik Babe khaya');
-})
+
+app.listen();
+
+// // Handling GET / Request
+// app.get('/', (req, res) => {
+//     res.send('Kelabo pratik Babe khaya');
+// })
   
 // Server setup
-app.listen(PORT,() => {
-    console.log('The application is listening '
-          + 'on port http://localhost:'+PORT);
-})
+// app.listen(PORT,() => {
+//     console.log(`The application is listening on port http://localhost:${PORT}`);
+// })
