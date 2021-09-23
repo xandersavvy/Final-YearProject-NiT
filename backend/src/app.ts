7598db0b40cfbc767007c606c3a1938a08dfe7c7
souvik ghosh <xandersavy@gmail.com>
2021-09-23 22:06:58 +0530

    changed to functional as instructed by lord pratik

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,76 +2,49 @@ import bodyParser = require('body-parser');
 import cookieParser = require('cookie-parser');
 import cors = require('cors');
 import logger = require('morgan');
-import mongoose from 'mongoose';
+// import mongoose from 'mongoose';
 import express = require('express');
 import  { NextFunction, Response, Request } from 'express';
 require('dotenv').config();
-export default class App {
-    public app: express.Application;
-    public apiV1Routes: express.Router;
-    
 
-    constructor() {
-        this.app = express();
-        this.apiV1Routes = express.Router();
-        this.initializeMiddlewares();
-        this.initializeLogger();
-        this.initializeErrorHandling();
-        this.routes();
-    }
+const app:express.Application = express();
+const apiRoutes:express.Router = express.Router();
 
-    // public createDBConnection() {
-    //     const mongoURI : string = process.env.MONGODB_URI as string ;
-    //     mongoose.connect(mongoURI,{
-    //             useNewUrlParser: true,
-    //             useUnifiedTopology: true,
-    //             useFindAndModify: true,
-    //             useCreateIndex: true,
-    //             poolSize: Number(process.env.MONGODB_POOLSIZE),
-    //             keepAlive: true,
-    //         });
-    // }
+const routes = () => {
+    app.get('/',(req: Request, res: Response, next: NextFunction) => {
+        res.send('Senhigans Backend');
+    })
+}
+const listen = () => {
+    routes();
+    app.listen(process.env.PORT , () => console.log(`App listening on port ${process.env.PORT}`)
+    )
+}
+const getServer = () => app;
 
-    public listen() {
-        // this.createDBConnection();
-        this.app.listen(process.env.PORT, () => {
-            console.log(`App listening on the port ${process.env.PORT}`);
-        });
-    }
+const initializeMiddlewares = () => {
+    app.use(cors());
+    app.use(bodyParser.json());
+    app.use(bodyParser.raw());
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(logger('[:date[web]] :method :url :status :res[content-length] - :remote-addr - :response-time ms'));
+}
 
-    public getServer() {
-        return this.app;
-    }
-
-    private initializeMiddlewares() {
-        this.app.use(cors());
-        this.app.use(bodyParser.json());
-        this.app.use(bodyParser.raw());
-        this.app.use(bodyParser.urlencoded({ extended: true }));
-        this.app.use(cookieParser());
-        this.app.use(logger('[:date[web]] :method :url :status :res[content-length] - :remote-addr - :response-time ms'));
-    }
-
-    private initializeErrorHandling() {
-
-    }
-
-    private initializeLogger() {
-        const LOG_PREFIX = new Date().getDate() + '.' + (new Date().getMonth() + 1) + '.' + new Date().getFullYear() + ' ' + new Date().getHours() + ':' + new Date().getMinutes() + ':' + new Date().getSeconds();
+const initializeLogger = () =>{
+    const LOG_PREFIX = new Date().getDate() + '.' + (new Date().getMonth() + 1) + '.' + new Date().getFullYear() + ' ' + new Date().getHours() + ':' + new Date().getMinutes() + ':' + new Date().getSeconds();
         const log = console.log;
         console.log = function () {
             const args = Array.from(arguments);
             args.unshift(LOG_PREFIX + ": ");
             log.apply(console, args);
         }
-    }
-
-    private routes() {
-        this.app.get('/', (req: Request, res: Response, next: NextFunction) => {
-            res.send('Senhigans Backend');
-        });
-        // this.app.use('/api/v1', this.apiV1Routes);
-        // this.apiV1Routes.use('/user', ); //enterroutes
-    }
 }
 
+
+
+export const App = {
+    routes,
+    listen ,
+    initializeLogger,
+    initializeMiddlewares
+}
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,21 +1,4 @@
-// Importing module
-// import express from 'express';
-import App from './app';
-require('dotenv').config();  
-// import cors from 'cors';
 
-const app = new App;
-const PORT : Number= 3000||process.env.PORT;
-  
+import {App} from './app';
 
-app.listen();
-
-// // Handling GET / Request
-// app.get('/', (req, res) => {
-//     res.send('Kelabo pratik Babe khaya');
-// })
-  
-// Server setup
-// app.listen(PORT,() => {
-//     console.log(`The application is listening on port http://localhost:${PORT}`);
-// })
+App.listen();
