ea97ea6418b57959a778b6463e4db0413ab24852
souvik ghosh <xandersavy@gmail.com>
2021-12-21 11:31:52 +0530

    no idea

--- frontend/pages/return.tsx
+++ frontend/pages/return.tsx
@@ -1,5 +1,4 @@
 import type { NextPage } from "next";
-import EmpInfoPage from "../components/empInfo/EmpInfoPage";
 import { ReturnPage } from "../components/return/ReturnPage";
 
 const Home: NextPage = () => <ReturnPage />
