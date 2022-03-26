506ffb845523152ecad293a34f3c9ba01f591c26
souvik ghosh <xandersavy@gmail.com>
2022-03-26 09:41:17 +0530

    Products

--- frontend/components/productStalk/ProductStalkForm.tsx
+++ frontend/components/productStalk/ProductStalkForm.tsx
@@ -5,19 +5,54 @@ import {
     Button    
 
 } from '@chakra-ui/react'
+import axios from 'axios';
+import { useState } from 'react';
+import { BASE_URL_PRODUCT } from '../constants';
 
 
 
 const formElements = [
-  ["Dealer Id","DealerId","text"],
-  ["Product Batch","batch","text"],
-  ["Product Code","code","text"],
-  ["Buying Price","bp","number"],
-  ["Maximum Retail Price","mrp","number"],
-  ["Product Count","count","number"]
+  ["Name","name","text"],
+  ["Buying Price","buyingPrice","number"],
+  ["M.R.P","mrp","number"],
+  ["Count","count","number"]
 ]
 
 export  const StalkForm = () => {
+  const [formData, setFormData] = useState({
+    "name": "",
+    "buyingPrice": "",
+    "mrp": "",
+    "count": ""
+  });
+  const [formErrors, setFormErrors] = useState(true);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  
+  const handleSubmit = async () =>{
+    setIsSubmitting(true);
+    const res = await axios.post(BASE_URL_PRODUCT,formData,{withCredentials:true}).then(res => {
+      console.log(res);
+      setIsSubmitting(false);}).catch(err => {
+        console.log(err);
+        setIsSubmitting(false);
+      });
+  console.log(res);
+    setFormData({
+      "name": "",
+    "buyingPrice": "",
+    "mrp": "",
+    "count": ""
+    });
+  }
+
+  const errorCheck = () => {
+    if( formData.name.length < 3 || formData.buyingPrice.length < 1 || formData.count.length < 1 || 
+        formData.mrp.length < 1 || formData.buyingPrice > formData.mrp ) setFormErrors(true);
+    else
+      setFormErrors(false);
+    }
+  
   
     return (
       <>
@@ -27,9 +62,18 @@ export  const StalkForm = () => {
                       <>
                       <FormLabel htmlFor={formElement[1]}>{formElement[0]}</FormLabel>
                       <Input type={formElement[2]} 
-                      placeholder={`Enter ${formElement[0]}`} 
+                      placeholder={`Enter ${formElement[0]} , type: ${formElement[2]}`} 
                       name={formElement[1]} 
+                      value={formData[formElement[1] as keyof typeof formData]}
                       isRequired
+                      onChange={(e) => {
+                        setFormData({
+                          ...formData,
+                          [formElement[1]]: e.target.value
+                        });
+                        errorCheck();
+                        console.log(formData);
+                      }}
                       />
                       </>
 
@@ -39,6 +83,9 @@ export  const StalkForm = () => {
               mt={4}
               colorScheme="teal"
               type="submit"
+              onClick={handleSubmit}
+              isLoading={isSubmitting}
+              isDisabled={formErrors}
             >
               Submit
             </Button>
